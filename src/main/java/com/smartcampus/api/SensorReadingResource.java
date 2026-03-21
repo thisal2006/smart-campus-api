a839b871d8c47e440822be832288f14499ebf482
thisal2006 <thisal.20231581@iit.ac.lk>
2026-03-21 11:02:17 +0000

    feat: create sensorreadingresource subresource for nested readings endpoints

---
+++ src/main/java/com/smartcampus/api/SensorReadingResource.java
@@ -0,0 +1,27 @@
+package com.smartcampus.api;
+
+import com.smartcampus.model.Sensor;
+import com.smartcampus.model.Reading;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SensorReadingResource {
+    private static final ConcurrentHashMap<Integer, Sensor> sensorStore = SensorResource.getSensorStore();
+    private int sensorId;
+
+    public SensorReadingResource(int sensorId) {
+        this.sensorId = sensorId;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getReadingHistory() {
+        Sensor sensor = sensorStore.get(sensorId);
+        if (sensor == null) {
+            return Response.status(404).entity("{\"error\": \"Sensor not found\"}").build();
+        }
+        return Response.ok(sensor.getReadings()).build();
+    }
+}
