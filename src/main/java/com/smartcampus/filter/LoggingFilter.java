bdbbb0a3f6e4fd178b187f1821c4204c74c4b441
thisal2006 <thisal.20231581@iit.ac.lk>
2026-03-23 14:52:09 +0000

    filter: implement containerrequestfilter and containerresponsefilter for request response logging

---
+++ src/main/java/com/smartcampus/filter/LoggingFilter.java
@@ -0,0 +1,24 @@
+package com.smartcampus.filter;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Provider
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+    private static final Logger logger = Logger.getLogger(LoggingFilter.class.getName());
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        logger.info("REQUEST: " + requestContext.getMethod() + " " + requestContext.getUriInfo().getRequestUri());
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        logger.info("RESPONSE: " + responseContext.getStatus());
+    }
+}
