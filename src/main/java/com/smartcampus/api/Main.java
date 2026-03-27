fa4b64270aff3a80bf944bed68dfe682dc81b953
thisal2006 <thisal.20231581@iit.ac.lk>
2026-03-27 11:55:33 +0000

    config: add configurable server port via system property default 8080

--- src/main/java/com/smartcampus/api/Main.java
+++ src/main/java/com/smartcampus/api/Main.java
@@ -4,15 +4,13 @@ import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import java.net.URI;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(
-                URI.create("http://localhost:8080/"),
-                new ApplicationConfig()
-        );
-        System.out.println("Server started at http://localhost:8080/api/v1");
-        System.out.println("Press Enter to stop...");
-        System.in.read();
-        server.shutdown();
-    }
+public static void main(String[] args) throws Exception {
+    String port = System.getProperty("server.port", "8080");
+    HttpServer server = GrizzlyHttpServerFactory.createHttpServer(
+            URI.create("http://localhost:" + port + "/"),
+            new ApplicationConfig()
+    );
+    System.out.println("Server started at http://localhost:" + port + "/api/v1");
+    System.in.read();
+    server.shutdown();
 }
