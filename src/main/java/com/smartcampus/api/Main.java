031f18309228b62214c8841943dade77e2c69d37
thisal2006 <thisal.20231581@iit.ac.lk>
2026-03-27 13:28:44 +0000

    enhance: add default uncaught exception handler for better error debugging

--- src/main/java/com/smartcampus/api/Main.java
+++ src/main/java/com/smartcampus/api/Main.java
@@ -5,6 +5,10 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import java.net.URI;
 
 public static void main(String[] args) throws Exception {
+    Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
+        System.err.println("Uncaught exception in thread " + thread.getName());
+        throwable.printStackTrace();
+    });
     String port = System.getProperty("server.port", "8080");
     HttpServer server = GrizzlyHttpServerFactory.createHttpServer(
             URI.create("http://localhost:" + port + "/"),
